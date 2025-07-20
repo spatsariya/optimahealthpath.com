9b8964d3e73aa604d98319f022903476a23eb965
Shivam Patsariya <s.patsariya@gmail.com>
2025-07-20 01:54:09 -0400

    Google Tag Manager

--- includes/header.php
+++ includes/header.php
@@ -231,8 +231,22 @@
             }
         }
     </style>
+
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P6RGH62J');</script>
+<!-- End Google Tag Manager -->
+
+
 </head>
 <body class="bg-gray-50">
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P6RGH62J"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <!-- Header -->
     <header class="fixed w-full z-50 px-8" style="top: 32px;">
         <nav class="container mx-auto px-6 py-4 backdrop-blur-lg bg-white/70 border border-gray-200 rounded-3xl">
